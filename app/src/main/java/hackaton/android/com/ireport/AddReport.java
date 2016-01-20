16105571c4946d9e2be2d4162efc73bb297913a7
Lady Toni Barretto <lady.barretto@cloudsherpas.com>
2016-01-20 16:45:20 +0800

    Add implementation for capture image

--- app/src/main/java/hackaton/android/com/ireport/AddReport.java
+++ app/src/main/java/hackaton/android/com/ireport/AddReport.java
@@ -1,16 +1,25 @@
 package hackaton.android.com.ireport;
 
+import android.app.Activity;
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
+import android.location.LocationManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.Toast;
@@ -21,7 +30,10 @@ import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
 import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
 import com.google.android.gms.location.LocationServices;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -36,6 +48,10 @@ public class AddReport extends AppCompatActivity implements ConnectionCallbacks,
     SimpleDateFormat sdf;
     GoogleApiClient mGoogleApiClient;
 
+    private LocationManager locationManager;
+    private Bitmap bitmap;
+    private ImageView imageView;
+    private static final int REQUEST_CODE = 1;
 
     @Override
     public void onConnectionSuspended(int n){
@@ -110,6 +126,15 @@ public class AddReport extends AppCompatActivity implements ConnectionCallbacks,
         setCurrentDate();
         setCurrentTime();
 
+        imageView = (ImageView) findViewById(R.id.imageView);
+        Button captureButton = (Button) findViewById(R.id.button_get_loc);
+        captureButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, REQUEST_CODE);
+            }
+        });
 
         if (mGoogleApiClient == null) {
             mGoogleApiClient = new GoogleApiClient.Builder(this)
@@ -210,6 +235,35 @@ public class AddReport extends AppCompatActivity implements ConnectionCallbacks,
         }
     }
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data){
 
+        InputStream stream = null;
+        if (requestCode == REQUEST_CODE && resultCode == Activity.RESULT_OK){
 
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            // CALL THIS METHOD TO GET THE URI FROM THE BITMAP
+            Uri tempUri = getImageUri(getApplicationContext(), photo);
+
+            // CALL THIS METHOD TO GET THE ACTUAL PATH
+            File finalFile = new File(getRealPathFromURI(tempUri));
+
+            System.out.println(finalFile);
+            imageView.setImageBitmap(photo);
+        }
+    }
+
+    private Uri getImageUri(Context inContext, Bitmap inImage) {
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        inImage.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+        String path = MediaStore.Images.Media.insertImage(inContext.getContentResolver(), inImage, "Title", null);
+        return Uri.parse(path);
+    }
+
+    private String getRealPathFromURI(Uri uri) {
+        Cursor cursor = getContentResolver().query(uri, null, null, null, null);
+        cursor.moveToFirst();
+        int idx = cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATA);
+        return cursor.getString(idx);
+    }
 }
